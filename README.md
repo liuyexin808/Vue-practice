ed99e4f1ae24f3f2816db47ad9587ff456a2e04f
liuyexin808 <liuyexin808@gmail.com>
2016-12-13 20:36:21 +0800

    fitst commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Vue-practice
 toDoList [demo](http://liuyexin.com/Vue-practice/toDoList/index.html) <br>
+search [demo](http://liuyexin.com/Vue-practice/search/index.html) <br>
---
+++ search-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+ <div id="app">
+  <search></search>
+</div>
+</template>
+
+<script>
+import search from './components/search-component.vue'
+export default {
+  name: 'app',
+  components:{
+    search
+  }
+}
+</script>
+
+<style>
+
+body {
+  height: 100vh;
+  background: url("./assets/background.png") no-repeat;
+  background-size: cover;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+[v-cloak] {
+  display: none;
+}
+
+</style>
